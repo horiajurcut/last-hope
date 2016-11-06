0185dae194a9aa98aaead029d3766e7474df2a02
Horia Jurcut <horia.dragos@gmail.com>
2016-11-06 21:09:05 +0100

    Implement TargetInteractiveItem class

---
+++ Assets/Scripts/Utils/TargetInteractible.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using VREngine.Core;
+
+namespace VREngine.Utils
+{
+    public class TargetInteractible : MonoBehaviour
+    {
+        [SerializeField]
+        private VRInteractiveItem m_InteractiveItem;
+        [SerializeField]
+        private Renderer m_Renderer;
+
+        private void OnEnable()
+        {
+            m_InteractiveItem.OnClick += ChangeColor;
+        }
+
+
+        private void OnDisable()
+        {
+            m_InteractiveItem.OnClick -= ChangeColor;
+        }
+
+        private void ChangeColor()
+        {
+            if (m_InteractiveItem.IsOver) {
+                m_Renderer.material.color = new Color(
+                   Random.Range(0f, 1f),
+                   Random.Range(0f, 1f),
+                   Random.Range(0f, 1f)
+               );
+            }
+        }
+    }
+}
+
